9cf9c49a4b91c495ac7304984700efc2b80dcf73
NORTH89 <nordintata0@gmail.com>
2024-05-30 12:42:00 +0100

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,38 @@ import {
   Button,
   Container,
   Flex,
-  Input,
-  InputGroup,
-  InputLeftElement,
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 import CreateUserModal from "./CreateUserModal";
-import { BiSearch } from "react-icons/bi";
-
-const Navbar = ({ setUsers }) => {
+import { useState, useEffect } from "react";
+import { BASE_URL } from "../App";
+const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch user data from your API
+    fetch(BASE_URL + "/friends")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setFilteredUsers(data); // Initially set filteredUsers to the fetched data
+      });
+  }, []);
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    const filtered = users.filter((user) =>
+      user.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  };
   return (
     <Container maxW={"900px"}>
       <Box
@@ -32,15 +51,17 @@ const Navbar = ({ setUsers }) => {
             gap={3}
             display={{ base: "none", sm: "flex" }}
           >
-            <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <BiSearch />
-              </InputLeftElement>
-              <Input
-                placeholder="Search for users"
-                onChange={(e) => setUsers(e.target.value)}
-              />
-            </InputGroup>
+            <input
+              type="text"
+              placeholder="Search users..."
+              value={query}
+              onChange={handleInputChange}
+            />
+            <ul>
+              {filteredUsers.map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+            </ul>
           </Flex>
           {/* Right side */}
           <Flex gap={3} alignItems={"center"}>
